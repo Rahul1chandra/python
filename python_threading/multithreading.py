36ea761021922e5eeb428d34177875c7793c0270
Rahul chandra <rahul1chandra@gmail.com>
2020-08-18 01:10:14 +0530

    updated

---
+++ python_threading/multithreading.py
@@ -0,0 +1,25 @@
+ 
+import threading
+import time
+class MyThreadClass:
+    
+    def display(self):
+        print (threading.current_thread().getName())
+        time.sleep(1)
+        i = 0
+        while i <11:
+            print (i)
+            i+=1
+
+class_obj = MyThreadClass()
+th_obj1 =  threading.Thread(target= class_obj.display)
+
+th_obj2 =  threading.Thread(target= class_obj.display)
+
+th_obj3 =  threading.Thread(target= class_obj.display)
+
+
+
+th_obj1.start()
+th_obj2.start()
+th_obj3.start()
